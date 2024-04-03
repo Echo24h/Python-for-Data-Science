f5a2185c586e1c5d43187a12b657661333e03810
Gregory Borne <gborne@c3r2p2.42nice.fr>
2024-04-03 21:45:44 +0200

    starting day 00

---
+++ Python - 0 - Starting/ex01/format_ft_time.py
@@ -0,0 +1,7 @@
+import time
+
+seconds_since_epoch = time.time()
+
+print(f"Seconds since January 1, 1970: {seconds_since_epoch:,.4f} or {seconds_since_epoch:.2e} in scientific notation")
+
+print(time.strftime("%b %d %Y", time.localtime(seconds_since_epoch)))
