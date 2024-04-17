b054d7bb7815d86c714dd55255e7001ef5015872
gborneGit <gregborne.pro@gmail.com>
2024-04-17 19:05:28 +0200

    add .vscode for flake8

--- Python - 2 - DataTable/ex01/aff_life.py
+++ Python - 2 - DataTable/ex01/aff_life.py
@@ -1,29 +1,34 @@
 import matplotlib.pyplot as plt
 from load_csv import load
-import matplotlib
 
-
-matplotlib.use('TkAgg')
 
 def draw_country_life(country: str):
-    # Charger le fichier CSV
+    """
+    Draw a line plot of the life expectancy of a country over the years.
+
+    Args:
+        country (str): The name of the country.
+
+    Returns:
+        None
+    """
+
     dataset = load("life_expectancy_years.csv")
 
     if dataset is not None:
-        # Filtrer les données pour obtenir les informations sur le pays de votre campus
-        country_data = dataset[dataset['country'] == country]
 
-        # Extraire les années et l'espérance de vie pour le pays donné
+        country_data = dataset[dataset['country'] == country]
         years = list(country_data.columns[1:])
         life_expectancy = country_data.values.tolist()[0][1:]
 
-        # Créer le graphique
-        plt.plot(years, life_expectancy, marker='o', linestyle='-')
-        plt.title(f"Life Expectancy in {country}")
+        plt.plot(years, life_expectancy)
+        plt.xticks(years[::40])
+        plt.title(f"{country} Life expectancy Projections")
         plt.xlabel("Year")
-        plt.ylabel("Life Expectancy (Years)")
-        plt.grid(True)
-        plt.show(block=True)  # Modifier ici pour afficher de manière bloquante
+        plt.ylabel("Life Expectancy")
 
-# Appeler la fonction pour dessiner le graphique pour le pays de votre campus
-draw_country_life("France")  # Remplacez "France" par le pays de votre campus
+        plt.show()
+
+
+if __name__ == "__main__":
+    draw_country_life("France")
---
+++ Python - 2 - DataTable/ex02/aff_pop.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+from load_csv import load
+
+
+def preprocess_population(pop_str):
+    """
+    Preprocesses the population string to convert it into
+    a numeric value in standard form.
+
+    Args:
+        pop_str (str): Population string with or without
+        the 'M' suffix for million.
+
+    Returns:
+        float: Numeric population value.
+    """
+    if pop_str.endswith("B"):
+        return float(pop_str[:-1]) * 1e9
+    elif pop_str.endswith("M"):
+        return float(pop_str[:-1]) * 1e6
+    elif pop_str.endswith("k"):
+        return float(pop_str[:-1]) * 1e3
+    else:
+        return float(pop_str)
+
+
+def draw_compare_countries(country1: str, country2: str):
+    """
+    Draw a line plot of the population of two countries over the years.
+
+    Args:
+        country1 (str): The name of the first country.
+        country2 (str): The name of the second country.
+
+    Returns:
+        None
+    """
+
+    dataset = load("population_total.csv")
+
+    if dataset is not None:
+
+        country1_data = dataset[dataset['country'] == country1]
+        country2_data = dataset[dataset['country'] == country2]
+
+        years = [str(year) for year in range(1800, 2051)]
+        country1_pop = country1_data[years].values.flatten()
+        country2_pop = country2_data[years].values.flatten()
+
+        country1_pop = [preprocess_population(pop) for pop in country1_pop]
+        country2_pop = [preprocess_population(pop) for pop in country2_pop]
+
+        plt.plot(years, country1_pop)
+        plt.plot(years, country2_pop)
+        plt.xticks(years[::40])
+        plt.title("Population Projections")
+        plt.legend([country1, country2], loc='lower right')
+        max_pop = max(max(country1_pop), max(country2_pop))
+
+        if max_pop > 1e9:
+            y_ticks = [i * 1e9 for i in range(int(max_pop / 1e9) + 1)]
+            plt.yticks(
+                y_ticks, ["{:,.0f}B".format(pop / 1e9) for pop in y_ticks])
+        elif max_pop > 1e6:
+            y_ticks = [i * 1e7 for i in range(int(max_pop / 1e7) + 1)]
+            plt.yticks(
+                y_ticks, ["{:,.0f}M".format(pop / 1e6) for pop in y_ticks])
+        else:
+            y_ticks = [i * 1e3 for i in range(int(max_pop / 1e3) + 1)]
+            plt.yticks(
+                y_ticks, ["{:,.0f}K".format(pop / 1e3) for pop in y_ticks])
+
+        plt.xlabel("Year")
+        plt.ylabel("Population")
+
+        plt.show()
+
+
+if __name__ == "__main__":
+    draw_compare_countries("France", "Belgium")
+    draw_compare_countries("India", "China")
